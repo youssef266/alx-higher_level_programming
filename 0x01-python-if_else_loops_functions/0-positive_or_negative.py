c1e952da91b71fbc956c5cbc01b0b6b1e76e790f
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-10-31 06:36:23 -0700

    alx

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import random
-n = random.randint(-10, 10)
-if n > 0:
-    print(f"{n} is positive")
-elif n < 0:
-    print(f"{n} is negative")
-else:
-    print(f"{n} is zero")
