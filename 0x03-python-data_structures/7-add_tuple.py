946475c4be27f5f6650e819491e9edf710baeaa3
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-11-05 06:12:32 -0800

    alx

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    lA = len(tuple_a)
+    lB = len(tuple_b)
+    if lA < 1:
+        tuple_a = (0, 0)
+    elif lA < 2:
+        tuple_a = (tuple_a[0], 0)
+
+
+    if lB < 1:
+        tuple_b = (0, 0)
+    elif lB < 2:
+        tuple_b = (tuple_b[0], 0)
+
+    result = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return result
