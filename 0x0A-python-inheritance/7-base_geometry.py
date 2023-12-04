484cd723b99909b592b360b242b73cc7d2488c98
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-04 20:01:37 +0200

    alx

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this model is for  a class BaseGeometry
+based on 6-base_geometry.py
+"""
+
+
+class BaseGeometry:
+    """this class based on 6-base_geometry.py"""
+    def area(self):
+        """an function"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """a function that validates the value
+        Args:
+            name: name in type str
+            value: the value of this name
+        Return:
+            TypeError if the vlaue is not integer,
+            ValueError if the value less than or equal 0
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer")
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0")
