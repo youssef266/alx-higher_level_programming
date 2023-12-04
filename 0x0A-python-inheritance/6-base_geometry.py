474c5721cf06b66e4fd72f3b8182bdc552297a88
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-04 19:46:18 +0200

    alx

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this model is for class based on 5-base_geometry.py"""
+
+
+class BaseGeometry():
+    """a class based on 5-base_geometry.py"""
+    def area(self):
+        """an function"""
+        raise Exception("area() is not implemented")
