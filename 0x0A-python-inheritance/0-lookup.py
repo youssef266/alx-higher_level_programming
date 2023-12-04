46ce907e4fe3cdf6a00d29780acc09a043b6a4e0
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-04 15:27:32 +0200

    alx

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This model return all the methods and attributes in an object"""
+
+
+def lookup(obj):
+    """return all available attributes and methods"""
+    return dir(obj)
