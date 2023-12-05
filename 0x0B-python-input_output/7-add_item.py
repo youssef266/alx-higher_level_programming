75d2086fac7165135723ab10a683e3008c6e8775
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-05 19:00:28 +0200

    alx

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""this model is for adds all arguments to a Python list
-and then save them to a file"""
-import sys
+"""Handles file input and saves to file"""
+from sys import argv
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-loade_form_json = __import__('6-load_from_json_file').load_from_json_file
 
 
-args = list(sys.argv[1:])
-
+argv.pop(0)
 try:
-    items = loade_form_json("add_item.json")
-except Exception:
-    items = []
-
-items.extend(args)
-save_to_json_file(items, "add_item.json")
+    deserialized = load_from_json_file("add_item.json")
+    if deserialized is None:
+        save_to_json_file(argv, "add_item.json")
+    else:
+        deserialized.extend(argv)
+        save_to_json_file(deserialized, "add_item.json")
+except FileNotFoundError:
+    save_to_json_file(argv, "add_item.json")
