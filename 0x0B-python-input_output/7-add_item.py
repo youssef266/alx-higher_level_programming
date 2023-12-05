3dd59c166db6ab31ad990935ce1f5e812c71a6f6
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-05 18:54:26 +0200

    alx

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""this model is for adds all arguments to a Python list
+and then save them to a file"""
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+loade_form_json = __import__('6-load_from_json_file').load_from_json_file
+
+
+args = list(sys.argv[1:])
+try:
+    items = loade_form_json("add_item.json")
+except FileNotFoundError:
+    items = []
+finally:
+    items.extend(args)
+    save_to_json_file(items, "add_item.json")
