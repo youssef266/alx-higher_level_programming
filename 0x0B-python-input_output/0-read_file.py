31498032fc75e1d683f4e4f490b8cfa94d2ca411
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-05 17:11:20 +0200

    alx

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,7 +2,15 @@
 """Contain a single function that read files"""
 
 
-def read_file(filename=""):
-    """Reads and prints content of file"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+def read_file(filename: str = "") -> None:
+    """
+    Read and print the contents of a file.
+
+    Args:
+        filename (str): The name of the file to be read.
+
+    Returns:
+        None
+    """
+    with open(filename, encoding="utf-8") as file:
+        print(file.read(), end="")
