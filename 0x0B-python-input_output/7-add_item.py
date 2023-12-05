79d812392d551041aa19829cdf0b0bcf5711fac2
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-05 19:07:22 +0200

    alx

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,11 +7,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 argv.pop(0)
 try:
-    deserialized = load_from_json_file("add_item.json")
-    if deserialized is None:
-        save_to_json_file(argv, "add_item.json")
-    else:
-        deserialized.extend(argv)
-        save_to_json_file(deserialized, "add_item.json")
-except FileNotFoundError:
-    save_to_json_file(argv, "add_item.json")
+    my_list = load_from_json_file("add_item.json")
+except  Exception:
+    my_list = []
+save_to_json_file(my_list + argv, "add_item.json")
