9ccb9e9443c0807586411fd43b7acffb9e8ed802
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-05 02:51:54 +0200

    alx

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""this model is to create a class name my list"""
+
+
+class MyList():
+    """print all the list but sorted"""
+    def print_sorted(self):
+        """print all the list sorted"""
+        print(sorted(self))
