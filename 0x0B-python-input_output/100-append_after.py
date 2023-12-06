d147791b2cefae165b71900d6f14e54199401e73
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-06 02:55:09 +0200

    alx

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """this model is for creating a function
 that inserts a line of text to a file
-after each line containing a specific string""" 
+after each line containing a specific string
+""" 
 
 
 def append_after(filename="", search_string="", new_string=""):
