7bcfd8f56703ee5b0962a618653af1110b614b2a
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-06 03:35:38 +0200

    alx

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 """this model is for writing a script
 line by line and computes metrics"""
+
+
+def print_stats(N, a, b, c):
+    """this model is for printing metrics"""
+    print("N: {}".format(N))
+    print("a: {}".format(a))
+    print("b: {}".format(b))
+    print("c: {}".format(c))
+    print("N/a + N/b - N/c: {}".format(N / a + N / b - N / c))
+    print("N/(a*b) + N/(a*c) + N/(b*c) - N/(a*b*c): {}".format(
+        N / (a * b) + N / (a * c) + N / (b * c) - N / (a * b * c)
+    ))
