d93fb02730897d46af091c5888215fef20d7790f
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-06 02:45:22 +0200

    alx

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,30 +1,28 @@
 #!/usr/bin/python3
-"""this module defines a class Student depending on 10-student"""
+"""Class defination of student"""
 
 
 class Student:
-    """class Student that defines a student
-    by first_name, last_name and age
-    """
+    """Defines a student class"""
     def __init__(self, first_name, last_name, age):
-        """initializes the class"""
+        """Initializes instance data"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student"""
+        """
+            Retrieves the dictionary representation of  student
+        """
+        dictionary = self.__dict__
         if attrs is None:
-            return self.__dict__
-        else:
-            new_dict = {}
-            for i in attrs:
-                if i in self.__dict__:
-                    new_dict[i] = self.__dict__[i]
-            return new_dict
-        
+            return dictionary
+
+        d = {k: v for k, v in dictionary.items() if k in attrs}
+        return d
+
     def reload_from_json(self, json):
         """
-        Replaces all attributes of the Student instance
+            Replaces all attributes of the Student instance
         """
         self.__dict__.update(json)
