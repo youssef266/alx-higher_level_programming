c7cb5e8eefad1784ca3b86f684ff54949c21f464
youssef266 <youssef.toti.taha.1212@gmail.com>
2023-12-06 03:36:44 +0200

    alx

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,15 +1,15 @@
-#!/usr/bin/env python3
-"""this model is for writing a script
-line by line and computes metrics"""
+#!/usr/bin/python3
+import random
+import sys
+from time import sleep
+import datetime
 
-
-def print_stats(N, a, b, c):
-    """this model is for printing metrics"""
-    print("N: {}".format(N))
-    print("a: {}".format(a))
-    print("b: {}".format(b))
-    print("c: {}".format(c))
-    print("N/a + N/b - N/c: {}".format(N / a + N / b - N / c))
-    print("N/(a*b) + N/(a*c) + N/(b*c) - N/(a*b*c): {}".format(
-        N / (a * b) + N / (a * c) + N / (b * c) - N / (a * b * c)
+for i in range(10000):
+    sleep(random.random())
+    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
+        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
+        datetime.datetime.now(),
+        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
+        random.randint(1, 1024)
     ))
+    sys.stdout.flush()
