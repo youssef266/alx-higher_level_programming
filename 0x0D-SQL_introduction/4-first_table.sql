f160d9258a947a5c7fda166389b1d57c32d51b0a
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-01-16 18:48:49 +0200

    alx

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- Creat table called first_table if not exist wih id,name
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT, 'name' VARCHAR(256));
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
