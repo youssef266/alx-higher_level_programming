a21b32c04b0099331996612bfeafe8dce0b5c07c
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-01-17 17:23:54 +0200

    alx

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- script for creat mysql server user user_0d_1.
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
