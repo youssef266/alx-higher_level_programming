c0c89a56019f50c29e233cea91a6e3db6e340a1d
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-13 21:35:17 +0200

    Alx

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const first_arg = require('fs');
+const fs = require('fs');
 
-const first_Arg = first_arg.readFileSync(process.argv[2]).toString();
-const second_Arg = first_arg.readFileSync(process.argv[3]).toString();
-fs.writeFileSync(process.argv[4], first_Arg + second_Arg);
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + sArg);
