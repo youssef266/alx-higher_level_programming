df9b8f37b417f645375354acfc4059af0660ea68
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-02-18 11:00:10 +0200

    Alx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,8 @@ import sys
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", user='root',
+                         passwd='Y$1youssef', db='hbtn_0e_0_usa', port=3306)
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
@@ -14,3 +14,6 @@ if __name__ == "__main__":
         print(row)
     cur.close()
     db.close()
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[3])
