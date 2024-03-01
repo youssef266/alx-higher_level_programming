882f117378bdf4739e6aafebd3b80464871b192f
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-03-01 15:01:54 +0200

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,8 +1,8 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """A script that:
-- takes in a URL and an email address,
-- sends a POST request to the passed URL with the email as a parameter,
-- and finally displays the body of the response.
+- takes in a URL,
+- sends a request to the URL and displays the value
+- of the X-Request-Id variable found in the header ofthe response.
 """
 import sys
 import urllib.request
