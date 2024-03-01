267d7b09d463b55221ffd27acc7da3b2dfa3f581
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-03-01 15:26:20 +0200

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,24 +1,20 @@
-#!/usr/bin/env python3
-"""A script that:
-- takes a repository name and owner name as arguments
-- sends a request to the GitHub API to retrieve the latest 10 commits
-- displays the commit id and the commit message
+#!/usr/bin/python3
+"""lists the 10 most recent commits on a given GitHub repository.
 """
-import requests
 import sys
+import requests
+
 
 if __name__ == "__main__":
-    repo_name = sys.argv[1]
-    owner_name = sys.argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
-    response = requests.get(url)
-
-    if response.status_code == 200:
-        commits = response.json()
-        for commit in commits:
-            commit_sha = commit["sha"]
-            commit_message = commit["commit"]["message"]
-            print(f"Commit: {commit_sha}\nMessage: {commit_message}\n")
-    else:
-        print(f"Error: {response.status_code}")
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
