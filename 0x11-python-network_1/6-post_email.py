607fdc8e20c8eb87cceaed2555fee076dd6a6265
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-03-01 15:00:26 +0200

    5-hbtn_header.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,14 +4,12 @@
 - sends a POST request to the passed URL with the email as a parameter,
 - and finally displays the body of the response.
 """
-import requests
 import sys
+import urllib.request
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    email = sys.argv[2]
 
-    payload = {'email': email}
-    response = requests.post(url, data=payload)
-
-    print(response.text)
+    request = urllib.request.Request(url)
+    with urllib.request.urlopen(request) as response:
+        print(dict(response.headers).get("X-Request-Id"))
