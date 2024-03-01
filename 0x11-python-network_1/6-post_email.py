86e2b8d17e160abc3a7f163382afdcaacbf9016d
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-03-01 15:01:12 +0200

    6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""A script that
+"""A script that:
 - takes in a URL and an email address,
 - sends a POST request to the passed URL with the email as a parameter,
 - and finally displays the body of the response.
