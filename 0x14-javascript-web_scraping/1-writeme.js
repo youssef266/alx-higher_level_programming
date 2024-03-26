ecf4ae8815bcb61e74edf6a43acce844545d7c47
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-03-26 19:33:51 +0200

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
 
-const writeFile = (filePath, content) => {
   fs.writeFile(filePath, content, 'utf-8', (err) => {
     if (err) {
       console.error(err);
     }
   });
-};
