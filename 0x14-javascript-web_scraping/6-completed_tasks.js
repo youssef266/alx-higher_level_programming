23dfe880eefd564da90d79dd4562aa1f47f324fb
youssef266 <youssef.toti.taha.1212@gmail.com>
2024-03-26 19:47:29 +0200

    4-starwars_count.js

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+const request = require('request');
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status code:', response.statusCode);
+  } else {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    todos.forEach((todo) => {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId]++;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
+      }
+    });
+
+    Object.entries(completedTasksByUser).forEach(([userId, count]) => {
+      console.log(`User ${userId} completed ${count} tasks`);
+    });
+  }
+});
